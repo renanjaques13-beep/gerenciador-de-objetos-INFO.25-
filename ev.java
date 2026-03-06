2344677e3805377aad0246a9a686cbc6e0426835
renanjaques13-beep <renanjaques13@gmail.com>
2026-03-05 21:45:55 -0300

    atividade - 1 / adivinhe um numero entre x aonde que mostra o quantidade de erros

---
+++ ev.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ev-1 
+{
+    public static void main(String[] args){
+        
+        Random aleatorio = new Random();
+        Scanner scan = new Scanner(System.in);
+
+        int valor = aleatorio.nextInt(30); 
+        int numero;
+        int tentativas = 0;
+
+        do {
+            System.out.print("Digite um número: ");
+            numero = scan.nextInt();
+            tentativas++;
+
+            if(numero < valor){
+                System.out.println("Palpite menor");
+            }
+            else if(numero > valor){
+                System.out.println("Palpite maior");
+            }
+            else{
+                System.out.println("Acertou!");
+            }
+            System.out.println("Número de tentativas: " + tentativas);
+             
+
+        } while(numero != valor);
+
+        System.out.println("Número de tentativas: " + tentativas);
+
+    
+    }
+}
